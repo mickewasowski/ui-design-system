9d02bbff8d80b133eff942ccc989e164612a981e
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-10-12 18:28:16 +0300

    edits

--- src/components/misc/loaders/Dots.tsx
+++ src/components/misc/loaders/Dots.tsx
@@ -9,10 +9,9 @@ interface IDotsProps {
 function Dots({size = 10, mainColor = 'black', subColor = 'darkgray'}: IDotsProps) {
     const dotStyle = {
         width: `${size}px`,
-        height: `${size}px`,
     };
 
-    return <div className="dots-loader" style={{gap: `${size / 10}px`}}>
+    return <div className="dots-loader" style={{gap: `${size / 5}px`}}>
         <p className="dot" style={{backgroundColor: subColor, ...dotStyle}}></p>
         <p className="dot" style={{backgroundColor: mainColor, ...dotStyle}}></p>
         <p className="dot" style={{backgroundColor: subColor, ...dotStyle}}></p>
