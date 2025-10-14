1e14a0ca5b75f1f6223d447b198fdf0f05a59dbf
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-10-14 21:34:34 +0300

    input skeleton

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,3 +3,60 @@
 // it should have states like inactive, focused, error
 // accept value and change handler
 // accept custom color
+
+import classNames from "classnames";
+
+interface IInputProps {
+  input: {
+    type: string;
+    value: string | number;
+    changeHandler: () => void;
+    attibutes?: React.InputHTMLAttributes<HTMLInputElement>;
+  };
+  label: {
+    text: string;
+    name?: string;
+  };
+  borderColor?: string;
+  textColor?: string;
+  inactive: boolean;
+  focused: boolean;
+  error: boolean;
+  errorText: string;
+}
+
+function Input({
+  input,
+  label,
+  borderColor,
+  textColor,
+  inactive,
+  focused,
+  error,
+  errorText,
+}: IInputProps) {
+  const {type, value, changeHandler, attibutes} = input;
+  const {text, name} = label;
+
+    const wrapperClasses = classNames({
+
+    });
+
+  const inputClasses = classNames({
+
+  });
+
+  const labelClasses = classNames({
+
+  });
+
+  return (
+    <div className={wrapperClasses}>
+      <p>{errorText}</p>
+      <input className={inputClasses} value={value} type={type} onChange={changeHandler} {...attibutes} />
+      <label className={labelClasses} name={name}>{text}</label>
+    </div>
+  );
+}
+
+export default Input;
